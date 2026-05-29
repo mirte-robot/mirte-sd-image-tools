1caae82653d63166cd13cbc9c2bd2eee305ebf56
Arend-Jan van Hilten <a.j.vanhilten@tudelft.nl>
2026-05-29 16:14:12 +0200

    Fix install.sh telemetrix download without special rolling release

--- installer/install.sh
+++ installer/install.sh
@@ -36,14 +36,25 @@ rm -rf picotool
 
 #  Download latest uf2 release, resulting in Telemetrix4RpiPico.uf2
 cd /root/ || exit 1
-# TODO:  Downlaods from arendjan/telemetrix4rpipico, as it isn't released yet on the official repo
-# curl -s https://api.github.com/repos/arendjan/telemetrix4rpipico/releases/latest |
-# 	grep ".*/Telemetrix4RpiPico.uf2" |
-# 	cut -d : -f 2,3 |
-# 	tr -d \" |
-# 	wget -qi -
-wget https://github.com/ArendJan/Telemetrix4RpiPico/releases/download/rolling/Telemetrix4RpiPico.uf2
 
+# download gh cli
+(type -p wget >/dev/null || (sudo apt update && sudo apt install wget -y)) &&
+	sudo mkdir -p -m 755 /etc/apt/keyrings &&
+	out=$(mktemp) && wget -nv -O$out https://cli.github.com/packages/githubcli-archive-keyring.gpg &&
+	cat $out | sudo tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null &&
+	sudo chmod go+r /etc/apt/keyrings/githubcli-archive-keyring.gpg &&
+	sudo mkdir -p -m 755 /etc/apt/sources.list.d &&
+	echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" | sudo tee /etc/apt/sources.list.d/github-cli.list >/dev/null &&
+	sudo apt update &&
+	sudo apt install gh -y
+
+gh run download --repo mirte-robot/telemetrix4rpipico --pattern "dist" -D .
+mv dist/Telemetrix4RpiPico.uf2 /root/Telemetrix4RpiPico.uf2 || true
+if [ ! -f /root/Telemetrix4RpiPico.uf2 ]; then
+	echo "Failed to download Telemetrix4RpiPico.uf2"
+	gh release download --clobber -D . --pattern "*" --repo mirte-robot/mirte-telemetrix4rpipico || true
+	mv Telemetrix4RpiPico.uf2 /root/Telemetrix4RpiPico.uf2 || true
+fi
 pip install git+https://github.com/arendjan/tmx-pico-aio.git@modules
 
 sudo apt autoremove && sudo apt clean
